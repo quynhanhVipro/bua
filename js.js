f9f71ebf027c17e90279a44daa20d1689cd3bbc7
quynhanhVipro <quynhanht092@gmail.com>
2024-08-11 23:38:26 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,30 +1,3 @@
-// const debounce = (callback, wait) => {
-//     let timer = null;
-//     return (...args) => {
-//       clearTimeout(timer);
-//       timer = setTimeout(
-//         () => callback.apply(null, args),
-//         wait
-//       );
-//     };
-//   };
-  
-//   const $mask = document.querySelector('.mask');
-//   const $uncensor = document.querySelector('.uncensor');
-//   const { clientWidth, clientHeight } = $uncensor;
-  
-// const handleMouseMove = debounce(
-//     (event) => {
-//       $mask.style.top = `${event.clientY - clientHeight / 2}px`;
-//       $mask.style.left = `${event.clientX - clientWidth / 2}px`;
-//     },
-//     10
-// );
-// document.addEventListener(
-//     'mousemove',
-//     handleMouseMove
-// );
-// const $mouseCursor = document.querySelector('.mouse-cursor');
 const debounce = (callback, wait) => {
   let timer = null;
   return (...args) => {
@@ -45,10 +18,12 @@ let isMouseDown = false;
 const handleMouseMove = debounce(
   (event) => {
       if (isMouseDown) {
-          const clientX = event.clientX || event.touches[0].clientX;
-          const clientY = event.clientY || event.touches[0].clientY;
-          $mask.style.top = `${clientY - clientHeight / 2}px`;
-          $mask.style.left = `${clientX - clientWidth / 2}px`;
+          const clientX = event.clientX || (event.touches && event.touches[0].clientX);
+          const clientY = event.clientY || (event.touches && event.touches[0].clientY);
+          if (clientX && clientY) {
+              $mask.style.top = `${clientY - clientHeight / 2}px`;
+              $mask.style.left = `${clientX - clientWidth / 2}px`;
+          }
       }
   },
   10
